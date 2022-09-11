87c7e8220c8ca07cd6486aaa3b292a0a248d3696
Godswill Okoye <chibuikemgodswill2@gmail.com>
2022-09-11 15:52:57 -0700

    print

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+
+	#include <stdio.h>
 	#include <stdlib.h>
 	#include <time.h>
 
@@ -7,16 +8,15 @@
 	 *
 	 * 		Return: Always 0 (Success)
 	 */
-	 
-	 		int main(void)
-	 		{
- 				char low;
 
-				for (low = 'a';low <= 'z'; low++)
-						putchar(low);
-					putchar('\n');
+	int main(void)
+		{
+				char low;
 
-						return (0);
-							}
-			
+					for (low = 'a'; low <= 'z'; low++)
+							putchar(low);
+						putchar('\n');
+
+							return (0);
+								}
 
