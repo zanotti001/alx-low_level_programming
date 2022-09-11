6dea4d67300de42a3d31f769b1ec20687bbde4dc
Godswill Okoye <chibuikemgodswill2@gmail.com>
2022-09-11 01:08:00 -0700

    comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,24 +1,28 @@
-#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
- * main	- Entry point
+ *  main - Entry point
  *
- * Return:Always 0 (Success)
+ *  Return: Always 0 (Success)
  */
+
 int main(void)
 {
-	int d = 48;
 
-	while (d < 58)
+	int d;
+
+	for (d = '0'; d <= '9'; d++)
 	{
+
 		putchar(d);
-		if (d!	= 57)
+		if (d != '9')
 		{
 			putchar(',');
-			putchar('');
+			putchar(' ');
 		}
-		d++;
 	}
 	putchar('\n');
-	return	(0);
+
+	return (0);
 }
