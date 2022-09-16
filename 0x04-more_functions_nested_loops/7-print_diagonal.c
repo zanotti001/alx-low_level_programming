489fd3e8324f99eadb226b11a181f2b75eb86efd
Godswill Okoye <chibuikemgodswill2@gmail.com>
2022-09-16 07:45:34 -0700

    zaza

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,20 +10,20 @@
 
 void print_diagonal(int n)
 {
-	int len, space;
+	int Ien, space;
 
 	if (n > 0)
 	{
-		for (len = 0; len < n; len++)
+		for (Ien = 0; len < n; Ien++)
 		{
-			for (space = 0; space < len; space++)
+			for (space = 0; space < Ien; space++)
 			{
 			putchar(' ');
 		}
 
 		putchar('\\');
 
-		if (len == (n - 1))
+		if (Ien == (n - 1))
 		{
 			continue;
 		}
