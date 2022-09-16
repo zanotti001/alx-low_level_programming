48dd0fb9e35984ea115782789ff0abfe830f8f25
Godswill Okoye <chibuikemgodswill2@gmail.com>
2022-09-15 20:15:51 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks whether a character is a digit or not
+ * @c: ttested character
+ * Return: 1 if it is, 0 otherwise
+ */
+
+int _isdigitt(int c)
+{
+	if ((c >= 48) && (c <= 57))
+	{
+		return (1);
+	}
+
+	return (0);
+}
