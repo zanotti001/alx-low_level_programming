9f0de05a1921c48f104482fe01882507a7669f5a
Godswill Okoye <chibuikemgodswill2@gmail.com>
2022-09-19 06:23:41 -0700

    zaza

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rev_string - printts given string in reverse
+ * @s: passed pointer argument for string
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int len = 0, i, length;
+	char c;
+
+	while (*(s + len) != '\0')
+	{
+
+		len++;
+	}
+	length = len;
+	len--;
+	for (i = 0; i < length / 2; i++)
+	{
+
+		c = *(s + i);
+		*(s + i) = *(s + len);
+		*(s + len) = c;
+		len--;
+	}
+}
