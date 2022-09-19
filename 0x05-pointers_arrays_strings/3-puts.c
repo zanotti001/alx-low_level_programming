31f9c2253e3f3843eabac8a7ecbaab108d0487e0
Godswill Okoye <chibuikemgodswill2@gmail.com>
2022-09-19 05:23:29 -0700

    zaza

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts - print a given string to screen
+ *
+ * @str: passed pointer argument for string
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+
+	int len = 0;
+
+	while (*(sttr + len) != '\0')
+	{
+		_putchar(*((str + len)) :
+				len++;
+				}
+				_putchar('\n');
+				}
