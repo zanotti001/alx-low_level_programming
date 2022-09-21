979de3de7b23c5496222742a7f31aa5ada51ee61
Godswill Okoye <chibuikemgodswill2@gmail.com>
2022-09-21 15:08:39 -0700

    zaza

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -16,7 +16,7 @@ char *_strncat(char *dest, char *src, int n)
 		dest_len++;
 
 	for (index = 0; src[index] && index < n; index++)
-		dest[dest__len++] = src[index];
+		dest[dest_len++] = src[index];
 
 
 	return (dest);
