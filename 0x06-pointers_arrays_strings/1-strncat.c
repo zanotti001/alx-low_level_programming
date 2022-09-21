b433b0ca9a446c872da0cfa7567c8990e2fc4d70
Godswill Okoye <chibuikemgodswill2@gmail.com>
2022-09-21 15:02:17 -0700

    zaza

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strncat - concatenate two strings but add inputed number of bytes
+ * @dest: string to be appened upon
+ * @src: string to be completed at end of dest
+ * @n:integer parameter to compare index to
+ * Return: returns new concatenated string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest__len++] = src[index];
+
+
+	return (dest);
+
+}
