b240a7e40f77f74e2e5cfa85d912c2e632eec0c8
Godswill Okoye <chibuikemgodswill2@gmail.com>
2022-09-21 13:55:17 -0700

    zaza

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,7 +2,7 @@
 
 /**
  * _strcat - concatenates the string pointed to by @src to
- * the end o the string pointed to by @dest
+ * the end of the string pointed to by @dest
  * @dest: String that will be appended
  * @src: String to be concatenated upon
  *
@@ -12,13 +12,14 @@
 char *_strcat(char *dest, char *src)
 {
 
+
 	int index = 0, dest_len = 0;
 
 	while (dest[index++])
 		dest_len++;
 
-	for (index = o; src[index]; index++)
-		dest[dest - len++] = src[index];
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
 
 	return (dest);
 
