1d13c5c8fce4e1f1452a58d8bf588f9360dcd77d
Godswill Okoye <chibuikemgodswill2@gmail.com>
2022-09-22 01:25:52 -0700

    zaza

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * string_toupper - changes all lowercase letters tto uppercase
+ * string_toupper - changes all lowercase letters to uppercase
  * @str: string to be changed
  *
  * Return: address tp the string
@@ -14,7 +14,7 @@ char *string_toupper(char *str)
 	while (str[i] != '\0')
 	{
 
-		if (str[i] >= 'a' && str[i] <= 'a')
+		if (str[i] >= 'a' && str[i] <= 'z')
 			str[i] -= 32;
 		i++;
 	}
