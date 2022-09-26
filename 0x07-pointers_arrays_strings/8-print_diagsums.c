2ac0b89295f6f2a7eefe9ef7a2ebdb135f01279e
Godswill Okoye <chibuikemgodswill2@gmail.com>
2022-09-26 15:01:03 -0700

    zaza

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -10,17 +10,17 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, j, p, 1 = 0, r = 0;
+	int i, j, p, l = 0, r = 0;
 
 	for (i = 0; i < size; i++)
 	{
 		p = (i * size) + i;
-		1 += *(a + p);
+		l += *(a + p);
 	}
 	for (j = 0; j < size; j++)
 	{
-		p = (j * size) + (size - 1 - j);
+		p = (j * size) + (size - l - j);
 		r += *(a + p);
 	}
-	printf("%i, %i\n", 1, r);
+	printf("%i, %i\n", l, r);
 }
