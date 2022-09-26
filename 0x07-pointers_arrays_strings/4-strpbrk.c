c21f64777261054a0dca1b1c4b13fcd802e4d280
Godswill Okoye <chibuikemgodswill2@gmail.com>
2022-09-26 10:27:26 -0700

    zaza

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,10 +11,12 @@ char *_strpbrk(char *s, char  *accept)
 {
 	unsigned int i, j;
 
-	for (i = 0; a[i]; i++)
+	for (i = 0; s[i]; i++)
 	{
-		if (s[i] == accept[j])
-			break;
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+				break;
 	}
 	if (accept[j])
 		return (s + i);
