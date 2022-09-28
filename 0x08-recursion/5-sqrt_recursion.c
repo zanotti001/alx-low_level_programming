061682cc33c11c71b729e922220ae3a4b2739ff4
Godswill Okoye <chibuikemgodswill2@gmail.com>
2022-09-28 09:51:09 -0700

    zaza

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,31 +1,21 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - returns the natural square root of a number
- * @n: number to be used
- *
- * Return: the square root of n
+ * _evaluate - evaluate function sqrt
+ * @i: integer
+ * @n: integer
+ * Return: evaluate sqrt
  */
-int _sqrt_recursion(int n)
-{
 
-if (n == 1 || n == 0)
-return (n);
-	return (_sqrt(0, n));
-	}
-
-/**
- * _sqrt - returns the square root of a number
- * @n: test number
- * @x: squared number
- *
- * Return: the square root of n
- */
-int _sqrt(int n, int x)
+int _evaluate(int i, intt n)
 {
-	if (n > x / 2)
-		return (-1);
-	else if (n * n == x)
+	/*Evaluate function*/
+	if (n == 0 || n == 1)
 		return (n);
-	return (_sqrt(n + 1, x));
+	else if (i * i < n)
+		return (i);
+
+	return (-1);
+
+	return (-1);
 }
