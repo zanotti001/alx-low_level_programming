4d05ef732e1ed35b111560506a1be17ef09762fa
Godswill Okoye <chibuikemgodswill2@gmail.com>
2022-09-28 07:33:15 -0700

    zaza

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,10 +4,10 @@
  * _sqrt_recursion - returns the natural square root of a number
  * @n: number to be used
  *
- * Return: the square rooot of n
+ * Return: the square root of n
  */
 int _sqrt_recursion(int n)
-	{
+{
 
 if (n == 1 || n == 0)
 return (n);
