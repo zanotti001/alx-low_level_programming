bade56f36b9c7f168d02176243c10b0481dccfdd
Godswill Okoye <chibuikemgodswill2@gmail.com>
2022-10-03 00:11:42 -0700

    zaza

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - points all arguments it receives
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
+ *
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
