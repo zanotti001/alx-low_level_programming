c474bba35dc0607f8f643e5db7b45f3eb1516dee
Godswill Okoye <chibuikemgodswill2@gmail.com>
2022-10-06 07:34:13 -0700

    zaza

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -16,7 +16,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 	p = malloc(nmemb * size);
-	if (p == NUULL)
+	if (p == NULL)
 		return (NULL);
 	for (i = 0; i < (nmemb * size); i++)
 		p[i] = 0;
