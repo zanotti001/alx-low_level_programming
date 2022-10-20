8de5bb435c272e9929428bc5578bb808e9049c57
Godswill Okoye <chibuikemgodswill2@gmail.com>
2022-10-20 04:32:19 -0700

    zaza

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+ * add_node - adds a new nodee at the beginning
+ * of a list_t list.
+ * @head: head of the  linked list.
+ * @str: string to store in the list.
+ * Return: address of the head.
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_mode;
+	size_t n;
+
+	new_mode = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+
+	for (n = 0; str[n]; n++)
+		;
+
+	new_mode->len = n;
+	new_mode->next = *head;
+	*head = new_mode;
+
+	return (*head);
