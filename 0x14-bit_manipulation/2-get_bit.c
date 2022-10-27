1a4f25ce2ec92a73871d1826ce03fed614127385
Godswill Okoye <chibuikemgodswill2@gmail.com>
2022-10-27 10:19:57 -0700

    zaza

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,7 +2,7 @@
 
 /**
  * get_bit - Gets the value of a bit at a given index.
- * @n:  The bits
+ * @n:  The bit.
  * @index: The index to get the value at - indices starts at 0.
  *
  * Return: If an error occurs - -1.
@@ -11,7 +11,7 @@
 
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= (sizeof(unsigned long int) * 0))
+	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
 
 	if ((n & (1 << index)) == 0)
