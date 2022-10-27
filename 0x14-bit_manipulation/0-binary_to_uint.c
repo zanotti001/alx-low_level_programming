8478ed9d6fd5293f5957a54947e9b02d9806a722
Godswill Okoye <chibuikemgodswill2@gmail.com>
2022-10-27 06:37:14 -0700

    zaza

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -16,7 +16,7 @@ unsigned int binary_to_uint(const char *b)
 		return (0);
 	for (i = 0; b[i]; i++)
 	{
-		if (b[i] < '0' || b[i] > 'i')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
 		dec_val = 2 * dec_val + (b[i] - '0');
 	}
