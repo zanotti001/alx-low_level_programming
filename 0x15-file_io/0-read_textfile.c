a2f197e7bf9e7ce0197f1c34c5f103eb8e0e4ff3
Godswill Okoye <chibuikemgodswill2@gmail.com>
2022-10-31 06:44:02 -0700

    zaza

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,11 +8,11 @@
  * function should read and print.
  *
  * Return: If the function fails or filename is NULL - 0.
- * o/w - the actual number of bytes the function can read and print.
+ * O/w - the actual number of bytes the function can read and print.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t 0, r, w;
+	ssize_t o, r, w;
 	char *buffer;
 
 	if (filename == NULL)
